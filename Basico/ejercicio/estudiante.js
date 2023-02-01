c5c30f658583f744743171fe4c2952df65668cff
Manuel Idrobo <feridrobo3@gmail.com>
2023-02-01 16:28:05 -0600

    actualizar ejercicio2

--- Basico/ejercicio/estudiante.js
+++ Basico/ejercicio/estudiante.js
@@ -11,36 +11,40 @@ class Estudiante {
         return this.mediaArray(this.calificaciones);
     }
     // Método
-    mediaArray(calificaciones) {
-        let valor = 0;
-        calificaciones.forEach(element => {
-             valor = valor + element;
-        });
-        valor = valor / calificaciones.length();
-        return valor;
+
+    mediaArray() {
+        const valor = this.calificaciones.reduce(
+            (accumulator, item) => accumulator = accumulator + item
+        );
+        let media = valor / this.calificaciones.length;
+        return media;
     }
         // Método
-    aprobo(nota) {
-        if(nota > 70){
-            return "el estudiante "+nombre+" aprobo"
+    aprobo() {
+        if(this.media > 70){ return true;
+        } else {
+            return false;
         }
     }
 }
 
 
 
-const  estudiante1 = new Estudiante("juan", 20, [2.0,3.3, 5.0]);
+const  estudiante1 = new Estudiante("juan", 20, [100.0,90.0, 80.0]);
 const  estudiante2 = new Estudiante("pedro", 18, [2.5,2.8, 4.5]);
 const  estudiante3 = new Estudiante("camilo", 25, [2.0,4.3, 4.3]);
 const  estudiante4 = new Estudiante("sandra", 33, [2.0,1.0, 4.2]);
 
 let estudiantes = new Array(estudiante1,estudiante2,estudiante3,estudiante4);
 
-for (let i = 0; i < estudiantes.length; i++) {
-    let estudiante = estudiantes[i];
-    let nota = estudiante.mediaArray(estudiante.calificaciones);
-    let resultado = estudiante.aprobo(nota);
-    if (resultado) {
+estudiantes.forEach(estudiante => {
+    let resultado = estudiante.aprobo();
+    
       console.log(resultado);
-    }
-  }
+
+      if(resultado){
+        console.log("Felicitaciones")
+      }else{console.log("Sigue intentando")}
+});
+
+
